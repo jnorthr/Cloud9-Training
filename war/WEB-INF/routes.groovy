6935224373074469c9aa91a94fa765eca7327ef4
jnorthr <james.b.northrop@googlemail.com>
2014-06-08 14:25:54 +0200

    add samples page to anynines using asciidoctor markup

--- war/WEB-INF/routes.groovy
+++ war/WEB-INF/routes.groovy
@@ -7,4 +7,5 @@ get "/download", forward: "/WEB-INF/pages/download.gtpl"
 get "/anynines.war",  forward: "/WEB-INF/downloads/anynines.war" 
 get "/anynines.zip",  forward: "/WEB-INF/downloads/anynines.zip" 
 get "/facts", forward: "/WEB-INF/pages/facts.gtpl"
+get "/samples", forward: "/anynines.html"
 
